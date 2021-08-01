88ccf4d1805b220b563a2e0d99f0b531efb5160a
Felipe Valenzuela <plansalud.chile@gmail.com>
2021-08-01 14:33:16 -0400

    configuracion  archivo settings

--- Plansalud/settings.py
+++ Plansalud/settings.py
@@ -10,11 +10,22 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -27,7 +38,9 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-STATICFILES_DIRS= ['C:/Users/fvale/OneDrive/Escritorio/Proyectos/Plansalud/Plansalud/static']
+
+
+#STATICFILES_DIRS= ['C:/Users/fvale/OneDrive/Escritorio/Proyectos/Plansalud/Plansalud/static']
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
@@ -97,7 +110,7 @@ WSGI_APPLICATION = 'Plansalud.wsgi.application'
 #}
 
 import dj_database_url 
-from decouple import config 
+from decouple import config
 
 DATABASES = {
     'default': dj_database_url.config(
